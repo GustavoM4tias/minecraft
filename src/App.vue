739f026169aa2aa1ba019bdd7b4846fec24b0c88
GustavoM4tias <128002171+GustavoM4tias@users.noreply.github.com>
2024-08-14 18:17:12 +0000

    install vue and adjust router

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  
+      <RouterLink to="/">Home</RouterLink>
+
+  <RouterView />
+
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {}
+</style>
